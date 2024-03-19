689d0ec55226c441b409e39f759089189ed6aa5e
mnihl <95353475+mnihl@users.noreply.github.com>
2024-03-19 11:13:06 +0100

    bugfix for sample code

--- start.py
+++ start.py
@@ -2,7 +2,4 @@ from src.app import create_app
 
 if __name__ == '__main__':
     app = create_app()
-    client = app.test_client()
-    response = client.get("/")
-    print(response)
     create_app().run(debug=False, port=7890)
